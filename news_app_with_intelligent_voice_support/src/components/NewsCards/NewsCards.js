1968f9d22408f28d9536aa2290f9e80bfad036b6
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2021-02-13 22:54:56 +0530

    news now displayed in cards

--- news_app_with_intelligent_voice_support/src/components/NewsCards/NewsCards.js
+++ news_app_with_intelligent_voice_support/src/components/NewsCards/NewsCards.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Grid, Grow, Typography } from '@material-ui/core';
 
 import NewsCard from '../NewsCard/NewsCard';
+import useStyles from './styles.js';
 
 const NewsCards = ({articles}) => {
+
+  const classes = useStyles();
+
   return (
-    <div>
-      {articles.map((article) => (
-        <NewsCard />
-      ))}
-    </div>
+    <Grow in>
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {articles.map((article, i) => (
+          <Grid key={i} item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex'}}>
+            <NewsCard article={article} i={i} />
+          </Grid>
+        ))}
+      </Grid>
+
+    </Grow>
   )
 }
 

e614e19a04a705fd05275385ed428c60afb9144a
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2021-02-13 21:00:45 +0530

    git added newsCards and newsCard components

--- news_app_with_intelligent_voice_support/src/App.js
+++ news_app_with_intelligent_voice_support/src/App.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+
+import NewsCards from './components/NewsCards/NewsCards';
 
 const alanKey = 'c642ce9329686c352c24d08a284376172e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
 
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command }) => {
-        if(command === 'testCommand'){
-          alert('This code was executed');
+      onCommand: ({ command, articles }) => {
+        if(command === 'newHeadlines'){
+          setNewsArticles(articles);
         }
       }
     })
@@ -18,6 +22,7 @@ const App = () => {
   return (
     <div>
       <h1>Alan AI News Application</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   )
 }
---
+++ news_app_with_intelligent_voice_support/src/components/NewsCards/NewsCards.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NewsCard from '../NewsCard/NewsCard';
+
+const NewsCards = ({articles}) => {
+  return (
+    <div>
+      {articles.map((article) => (
+        <NewsCard />
+      ))}
+    </div>
+  )
+}
+
+export default NewsCards;

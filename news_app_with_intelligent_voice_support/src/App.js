159c2edfb8e5a65792798f343a19c4590415b59e
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2021-02-14 00:14:07 +0530

    added logo and styles

--- news_app_with_intelligent_voice_support/src/App.js
+++ news_app_with_intelligent_voice_support/src/App.js
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
+
+import alan from './alan.jpg';
 
 const alanKey = 'c642ce9329686c352c24d08a284376172e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
 
   const [newsArticles, setNewsArticles] = useState([]);
+
+  const classes = useStyles();
 
   useEffect(() => {
     alanBtn({
@@ -21,7 +26,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>Alan AI News Application</h1>
+      <div className={classes.logoContainer}>
+        <img src={alan} className={classes.alanLogo}/>
+      </div>
       <NewsCards articles={newsArticles} />
     </div>
   )
